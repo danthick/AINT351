e4e72143c1bf60d5fb9a9623e7bab3ae0f9e2e62
Dan Thick <dan.thick@hotmail.co.uk>
2019-11-27 15:54:23 +0000

    Refactoring code. Updating graphs. Started section 1 of report.

--- DisplayRevoluteArm.m
+++ DisplayRevoluteArm.m
@@ -7,10 +7,20 @@ function DisplayRevoluteArm()
     baseOrigin = [0, 0];
     samples = 1000;
     
-    % Generating 2xsamples data between 0 - pi
-    data = pi * rand(2,samples);
-    [P1, P2] = RevoluteForwardKinematics2D(armLength, data, baseOrigin);
-    % Plot data
+    % Generating 2 x samples between 0 - pi
+    angles = pi * rand(2,samples);
+    % Run angles through forward kinematics
+    [P1, P2] = RevoluteForwardKinematics2D(armLength, angles, baseOrigin);
+    
+    % Plot randomly generated angles
+    figure
+    hold on
+    title({'ID: 10555972', 'Arm Joint Angles'});
+    xlabel('x-axis');
+    ylabel('y-axis'); 
+    plot(angles(1,:), angles(2,:), 'r.');
+    
+    % Plot end points
     figure
     hold on
     title({'ID: 10555972', 'Arm Endpoint'});
@@ -18,18 +28,17 @@ function DisplayRevoluteArm()
     ylabel('y[m]'); 
     plot(P2(1,:), P2(2,:), 'r.')
     plot(baseOrigin(1), baseOrigin(2), 'b.', 'MarkerSize', 20);
-
     
-    % Generating just 10 samples
-    samples = 10;
-    data = pi * rand(2,samples);
-    [P1, P2] = RevoluteForwardKinematics2D(armLength, data, baseOrigin);
-        
+    % Plot 10 arm configurations
     figure
-    for i = 1:samples
-      % Plotting points with arm, connecting to origin
+    title({'ID: 10555972', 'Arm Configuration'});
+    xlabel('x[m]');
+    ylabel('y[m]');
+    for i = 1:10
       hold on
+      % Plotting from elbow to end of arm
       plot([P1(1,i) P2(1,i)],[P1(2,i) P2(2,i)], 'b-o', 'MarkerSize', 5,  'MarkerFaceColor', 'green');
+      % Plotting from origin to elbow
       plot([P1(1,i) baseOrigin(1)], [P1(2,i) baseOrigin(2)], 'r-o', 'MarkerSize', 5);
     end
 end
